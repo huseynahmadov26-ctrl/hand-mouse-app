416de129ccf37752ad4e9a0f7803a258cf9d9fcf
Zero <huseynahmadov26@gmail.com>
2026-05-17 12:25:03 -0700

    Fix CameraX version for GitHub build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 }
 
 dependencies {
-    val cameraxVersion = "1.5.2"
+    val cameraxVersion = "1.4.2"
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
